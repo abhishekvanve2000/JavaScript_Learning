b0816db50f4fb351c68935019e776871b032b127
abhishekvanve2000 <abhishekvanve2000@gmail.com>
2025-01-30 18:38:12 +0530

    typeOf Assignment
    practiced  typeOf operator  with various type of values

---
+++ 03_typeOfAssign.js
@@ -0,0 +1,22 @@
+var bankName;
+console.log('Bank NAme',bankName,'Type Of Bank Name',typeof bankName);          //before initilization
+bankName='SBI Bank'; 
+console.log('Bank Name ',bankName,'type Of Bank Name ',typeof bankName);        // After initilization
+
+
+var marks = '90%';
+console.log('marks=>',marks,'Type Of Marks',typeof marks);
+
+
+
+var isWorking;
+console.log('is Working=>',isWorking,'TYpe Of Is Working=>',typeof isWorking);
+isWorking=true;
+console.log('is Working=>',isWorking,'Type Of Is Working=>',typeof isWorking);
+
+
+var totalCount='Hundread And Seaven';
+console.log('Total Count=>',totalCount,'Type Of Total Count',typeof totalCount);
+var totalCount=107;
+console.log('Total count=>',totalCount,'type Of TotalCount',typeof totalCount);
+
