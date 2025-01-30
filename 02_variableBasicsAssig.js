00b25c41bbe7d90ae53c98e5c6780015c7454d97
abhishekvanve2000 <abhishekvanve2000@gmail.com>
2025-01-30 18:37:19 +0530

    Basic Variable Assignment
    practiced  variable declaration and initialization

---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,13 @@
+
+
+var namea = 'Abhisehk vanve';   //string
+console.log('Name=>',namea,'Type Of Datatype',typeof namea);
+
+
+var ange =24;           //Number
+console.log("Age=>",ange,"Type Of Age=>",typeof ange);
+
+
+var isStuding= true;        //boolean
+console.log('is Studying=>',isStuding,'Type Of Is Studying=>', typeof isStuding);
+
