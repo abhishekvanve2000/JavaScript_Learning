623aa40278d45478eb9141baa14bd3e657da7d34
abhishekvanve2000 <abhishekvanve2000@gmail.com>
2025-01-30 18:34:18 +0530

    Variable Assignment
    Practiced some basic variable problems

---
+++ 01_variableAssign.js
@@ -0,0 +1,14 @@
+
+var myFullname;                                         //Variable decleration
+myFullname='abhishek vanve';                            //value initilization
+console.log('before update=>',myFullname);              //logging value on console
+myFullname='Abhishek Babanraw Vanve';                   //updating varibale
+console.log('After Updating Full Name=>',myFullname);   // logging value after updation    
+
+var mylogve='i love javascript';
+console.log(mylogve);
+
+
+var firstName='abhishek', lastName='vanve',age='24',address='Beed',pincode='431122',gender='male',isMarried='false';
+console.log('first name=',firstName,'last NAme=',lastName,'Age=',age,'Adreass=',address,'Picode=',pincode,'gendar=',gender,'Married=',isMarried);
+
