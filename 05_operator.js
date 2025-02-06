14f91d92fac1410eaa3a891d450af20f7cab26b9
abhishekvanve2000 <abhishekvanve2000@gmail.com>
2025-02-06 21:31:11 +0530

    Update 05_operator.js
    Practiced Comparison and logical operaters

--- 05_operator.js
+++ 05_operator.js
@@ -73,3 +73,90 @@ console.log(`Result is- ${num3}`);
 
 
 
+
+
+
+console.log('_______________________________________________________________________________');
+
+
+
+
+console.log('_____Comparison_Operator_____');
+var num5 = 20;
+var num6 = '20';
+
+
+console.log('_Eqal ==_');
+var result = num5 == num6;
+console.log(`Result is- ${result}`);            //covernts to same type before conversion
+
+
+console.log('_Strict Eqal ===_');
+var result = num5 === num6;
+console.log(`Result is- ${result}`);            //checks if the value of value and is type is same 
+
+console.log('_Not Eqal !=_');
+var result = num5 != num6;
+console.log(`Result is- ${result}`);
+
+console.log('_Strict Not Eqal !==_');
+var result = num5 !== num6;
+console.log(`Result is- ${result}`);            // prints true (Diffrent Data Types)
+
+
+console.log('_Greater Than >_');
+var result = num5 > num6;                       //Cheks If left value is greater than right value
+console.log(`Result is- ${result}`);            //False
+
+console.log('_Smaler Than <_');
+var result = num5 < num6;                       //Cheks If left value is Smaller than right value
+console.log(`Result is- ${result}`);            //False
+
+console.log('_Greater Than Equal >=_');
+var result = num5 >= num6;                       //Cheks If left value is Grearter than or Equal right value
+console.log(`Result is- ${result}`);            //True
+
+console.log('_Smaller Than Equal <=_');
+var result = num5 <= num6;                       //Cheks If left value is Smaller than or Equal right value
+console.log(`Result is- ${result}`);            //True
+
+
+
+
+
+
+
+
+console.log('_______________________________________________________________________________');
+
+
+console.log('_____Logical_Operator_____');
+var num5 = 20;
+var num6 = '20';
+
+
+
+console.log('_AND &&_');                        //Both COnditions Need To Be True In AND Operator
+var result = num6 < num5 && num5 == num6;
+console.log(`Result is- ${result}`);            //False (First Constion Is False)
+
+
+console.log('_OR ||_');                        //One  Of Two COnditions Need To Be True In OR Operator
+var result = num6 < num5  || num5 == num6;
+console.log(`Result is- ${result}`);            //False (First Constion Is False)
+
+
+console.log('_Not !_');                        //Converts "True" Into "False" And "False" Into "True"
+var result = ! num5 == num6;
+console.log(`Result is- ${result}`);            //False     COnverts True Into False 
+
+
+
+
+
+
+var pin = '4490';
+console.log(typeof pin);
+
+var resultp = + pin ; 
+console.log(pin   ,typeof resultp);
