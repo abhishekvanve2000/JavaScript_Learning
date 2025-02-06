347529df8219f0ef8da4c721df26b12689f4cf38
abhishekvanve2000 <abhishekvanve2000@gmail.com>
2025-02-06 21:32:23 +0530

    Created 05_ternaryOperator.js
    Learned about ternary operator and practiced it with one example

---
+++ 05_ternaryOperator.js
@@ -0,0 +1,12 @@
+
+
+
+//Syntax
+//  Condition ? "Value If True" : "Value If False";
+
+
+var jennyAge = 18;
+var result = jennyAge >= 18 ? "Can Vote" : "Can Not Vote" ;
+
+console.log(`Result is- ${result}`);
+
