abcbf5dfd91d17489bb1a6c8243de7b974a493de
abhishekvanve2000 <abhishekvanve2000@gmail.com>
2025-02-09 20:12:27 +0530

    lterative statements
    Learned forLoop

---
+++ 06_forLoop.js
@@ -0,0 +1,47 @@
+
+
+
+// for 0 to 10
+
+for (let index = 0; index <=10; index++) {
+    console.log(index);
+    
+}
+
+
+//table of 5
+ for (let index = 5; index <=50 ; index=index+5) {
+    console.log(index);   
+ }
+
+
+
+ //for 10 to 0 
+ for (let index = 10; index >=1; index--) {
+    console.log(index);
+    
+ }
+
+ // 7 table in reverse order
+ for (let index = 70; index >=7; index=index-7) {
+   console.log(index);   
+ }
+
+
+
+ //Infinite Loop
+
+ /* for (let index = 10; index >0; index++) {
+    console.log(index);   
+ } */
+
+
+
+
+    //While Loop
+    var num=1;
+    while (num <=10) {
+      console.log(num);
+      num++    
+      
+    }
