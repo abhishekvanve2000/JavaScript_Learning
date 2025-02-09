50251b781204056ff6b7c70dfe1c66ec1487cd84
abhishekvanve2000 <abhishekvanve2000@gmail.com>
2025-02-09 20:12:51 +0530

    Jump statements
    Break/Continue

---
+++ 06_jumpStatement.js
@@ -0,0 +1,33 @@
+
+
+//Break
+
+for (let index = 0; index < 10; index++) {
+    console.log(`Iterration Start`);
+    
+    if (index ==6) {
+        break;
+    }
+    console.log(`Value Is-${index}  Iteration is ${index+1}`);
+    console.log(`---End Iteration---`);
+
+}
+
+
+
+
+
+
+
+//COntinue 
+
+for (let index = 0; index < 10; index++) {
+    console.log(`Iterration Start`);
+    
+    if (index ==6) {
+        continue;
+    }
+    console.log(`Value Is-${index}  Iteration is ${index+1}`);
+    console.log(`---End Iteration---`);
+
+}
