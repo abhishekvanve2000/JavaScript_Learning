cf1449e4dafe82ddfcfeba3154318484f84edfed
abhishekvanve2000 <abhishekvanve2000@gmail.com>
2025-03-07 16:07:20 +0530

    Created 09_object.js
    Created object.js .learned about how to create object and ow to use dot notation/bracket notation

---
+++ 09_object.js
@@ -0,0 +1,17 @@
+let person={
+    RollNumbwer:421,
+    emName:`Abhishek`,
+    state:`Maha`,
+    isMarried:false,
+    city:`Beed`,
+}
+
+
+console.log(person);
+console.log(`Type Of Person is-${typeof person}`);
+
+
+console.log(`Roll Number Is ${person.RollNumbwer}`);
+console.log(`City Of Student is-${person[`city`]}`);
+
+
