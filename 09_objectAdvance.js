0e63225994ec90e43be61f112d1ea9960f1a7a57
abhishekvanve2000 <abhishekvanve2000@gmail.com>
2025-03-11 22:32:23 +0530

    created 09_objectAdvance.js
    created object with const and learned to how to access the key

---
+++ 09_objectAdvance.js
@@ -0,0 +1,22 @@
+
+
+const student ={
+    name:`abc`,
+    rollNumber:6556,
+    city:`Beed`,
+    collage:`Aditya Collage`
+}
+
+student.city=`Mumbai`;
+console.log(student);
+
+
+// student=person;             //Not Allowed
+
+
+const person={
+    empId:5566,
+    city:`Mumbai`,
+    company:`Infosys`,
+    desigination:`Software Engineer`
+}
