c4a27a49369072220e1e7371c5bbe6b75176b4b5
abhishekvanve2000 <abhishekvanve2000@gmail.com>
2025-03-22 14:31:08 +0530

    create  10_mergedObject.js
    merged object using object.assign(); method

---
+++ 10_mergedObject.js
@@ -0,0 +1,17 @@
+
+
+let jenny={
+    fullname:`jenny`,
+    age:27,
+    city:`Beed`,
+    college:`GPB`,
+   
+}
+let anna={
+    math:30,
+    Science:40,
+    Biology:80
+}
+
+let merged=Object.assign({},jenny,anna);
+console.table(merged);
